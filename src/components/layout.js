5aa1ff6ade143c0c87367efc25a86066fe7a03de
Shivangi Sareen <s_sareen@apple.com>
2021-10-30 12:58:06 +0100

    remove <Header> component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,20 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-import Header from "./header";
 import { container, title, navBar, navLink, content } from "./layout.module.css";
 
 const Layout = ({siteTitle, subTitle, children}) => {
 
     return (
         <div className={container}>
-            <Header/>
-            <nav className={navBar}>
-                <Link to="/" className={navLink}>Home</Link> 
-                <Link to="/blog" className={navLink}>Blog</Link>
-                <Link to="/about" className={navLink}>About</Link>
-            </nav>
-            <h3>{subTitle}</h3>
+            <div>
+              <nav className={navBar}>
+                  <Link to="/" className={navLink}>Home</Link> 
+                  <Link to="/blog" className={navLink}>Blog</Link>
+                  <Link to="/about" className={navLink}>About</Link>
+              </nav>
+              <h3>{subTitle}</h3>
+            </div>
             <main className={content}>
               {children}
             </main>
