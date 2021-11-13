e8998823c605d8439c660448dec53649753b4ae4
Shivangi Sareen <s_sareen@apple.com>
2021-11-13 22:00:42 +0000

    add links to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,10 @@ const AboutPage = () => {
             <br/>
             <h2 className={text}>find me here</h2>
             <div className={links}>
-                links go here
+                <a href="https://www.linkedin.com/in/shivangisareen/" target="_blank">linkedin</a> {"\t"}
+                <a href="https://twitter.com/SareenShivangi" target="_blank">twitter</a> {"\t"}
+                <a href="https://medium.com/@shivangisareen" target="_blank">medium</a> {"\t"}
+                <a href="https://shivangisareen.portfoliobox.net" target="_blank">portfolio</a>
             </div>
         </div>
     );
