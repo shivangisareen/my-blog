8514635355d9e1e36ad5bc2970c95fcd079f91fb
Shivangi Sareen <s_sareen@apple.com>
2021-11-21 21:49:41 +0000

    add content to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,6 +16,8 @@ const AboutPage = () => {
 
             <div className={text}>
                 I graduated in 2019 from Trinity College Dublin with a bachelors in computer engineering. I'm currently working at Apple in the AI/ML team for Siri Understanding.
+                <br/>
+                Previously, I worked at American Express, in a range of back-end, front-end and devops roles.
             </div>
 
             
