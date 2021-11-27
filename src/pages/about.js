5579eeac5edf02425cf8a59bddeef7a639faf851
Shivangi Sareen <s_sareen@apple.com>
2021-11-27 17:03:29 +0000

    update about page and styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import NavBar from "../components/navBar";
 import Header from "../components/header";
 
-import { text } from "./about.module.css";
+import { text, specialText } from "./about.module.css";
 
 const AboutPage = () => {
     return (
@@ -15,9 +15,9 @@ const AboutPage = () => {
             <br/>
 
             <div className={text}>
-                I graduated in 2019 from Trinity College Dublin with a bachelors in computer engineering. I'm currently working at Apple in the AI/ML team for Siri Understanding.
-                <br/>
-                Previously, I worked at American Express, in a range of back-end, front-end and devops roles.
+                I graduated in 2019 from <span className={specialText}>Trinity College Dublin</span> with a bachelors in computer engineering. I'm currently working at <span className={specialText}>Apple</span> in the AI/ML team for Siri Understanding.
+                <br/> <br/>
+                Previously, I worked at <span className={specialText}>American Express</span>, in a range of back-end, front-end and devops roles.
             </div>
 
             
