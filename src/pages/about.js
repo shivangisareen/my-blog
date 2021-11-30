e0c0a890a16dd22cb5f85a4065853df16630e9fb
Shivangi Sareen <s_sareen@apple.com>
2021-11-30 20:19:45 +0000

    edit about page content, delete a blog post

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,11 +13,11 @@ const AboutPage = () => {
             <br/>
             <br/>
             <br/>
-
             <div className={text}>
-                I graduated in 2019 from <span className={specialText}>Trinity College Dublin</span> with a bachelors in computer engineering. I'm currently working at <span className={specialText}>Apple</span> in the AI/ML team for Siri Understanding.
+                <span className={specialText}>Hi!</span> I graduated in 2019 from <span className={specialText}>Trinity College Dublin</span> with a bachelors in computer engineering. I'm currently working at <span className={specialText}>Apple</span> in the AI/ML team for Siri Understanding.
                 <br/> <br/>
-                Previously, I worked at <span className={specialText}>American Express</span>, in a range of back-end, front-end and devops roles.
+                Previously, I worked at <span className={specialText}>American Express</span>, in a range of back-end, front-end and devops roles. <br/>
+                
             </div>
 
             
