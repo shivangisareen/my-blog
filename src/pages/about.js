92933e88a30058c1f24e9ae918ac2ebf65efbe2b
Shivangi Sareen <s_sareen@apple.com>
2021-12-03 14:52:13 +0000

    edit content

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,13 +13,16 @@ const AboutPage = () => {
             <br/>
             <br/>
             <p className={text}>
-                <span className={specialText}>Hi!</span> I'm a software engineer at <span className={specialText}>Apple</span> in the AI/ML team for Siri Understanding.
+                <span className={specialText}>Hi!</span> I'm a software engineer at <span className={specialText}>Apple</span> in the <span className={specialText}>AI/ML</span> team for <span className={specialText}>Siri Understanding</span>.
                 <br/> 
-                <br/>
                 Previously, I worked at <span className={specialText}>American Express</span>, in a range of back-end, front-end and devops roles. 
                 I graduated in 2019 from <span className={specialText}>Trinity College Dublin</span> with a bachelors in computer engineering. 
                 <br/>
-                I've taught coding courses at <span className={specialText}>Code First Girls</span>, namely Introduction to Data and SQL Programming and a Nanodegree course - Data Analytics with Python and SQL.
+                I've taught <span className={specialText}>SQL</span> and <span className={specialText}>Python</span> courses at <span className={specialText}>Code First Girls</span>, to train women in coding skills. 
+                I'm a <span className={specialText}>UN Volunteer</span> and have worked with a number of incredible organisations inlcuding <span className={specialText}>UNDP</span>, <span className={specialText}>International Telecommunication Union</span> and <span className={specialText}>Surfrider Foundation Europe</span>.
+                <br/>
+                I love creating digital illustrations, reading and writing. And I listen to podcasts all day everyday. 
+                <span className={specialText}> Fun fact:</span> I am one of the <span className={specialText}>co-authors</span> of a case book exploring <span className={specialText}>social enterprises</span> in <span className={specialText}>Ireland</span> and <span className={specialText}>Vietnam</span>. 
 
             </p>
         </div>
