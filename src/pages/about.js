08f6441da6c7be49b34f9223fe8c90064f48c252
Shivangi Sareen <s_sareen@apple.com>
2021-12-12 12:56:01 +0000

    fix typo in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,7 +19,7 @@ const AboutPage = () => {
                 I graduated in 2019 from <span className={specialText}>Trinity College Dublin</span> with a bachelors in computer engineering. 
                 <br/>
                 I've taught <span className={specialText}>SQL</span> and <span className={specialText}>Python</span> courses at <span className={specialText}>Code First Girls</span>, to train women in coding skills. 
-                I'm a <span className={specialText}>UN Volunteer</span> and have worked with a number of incredible organisations inlcuding <span className={specialText}>UNDP</span>, <span className={specialText}>International Telecommunication Union</span> and <span className={specialText}>Surfrider Foundation Europe</span>.
+                I'm a <span className={specialText}>UN Volunteer</span> and have worked with a number of incredible organisations including <span className={specialText}>UNDP</span>, <span className={specialText}>International Telecommunication Union</span> and <span className={specialText}>Surfrider Foundation Europe</span>.
                 <br/>
                 I love creating digital illustrations, reading and writing. I listen to podcasts all day everyday. 
                 <span className={specialText}> Fun fact:</span> I am one of the <span className={specialText}>co-authors</span> of a case book exploring <span className={specialText}>social enterprises</span> in <span className={specialText}>Ireland</span> and <span className={specialText}>Vietnam</span>. 
