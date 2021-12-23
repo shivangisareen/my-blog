666155adfd5049bf4ddc5ef7b64d0a8b5d802bb5
Shivangi Sareen <s_sareen@apple.com>
2021-12-23 14:45:17 +0000

    remove commented out code and unused imports

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { graphql, Link } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 import NavBar from "../../components/navBar";
 import Header from "../../components/header";
@@ -9,7 +8,6 @@ import { content, title, goBack, datePosted, text } from "./blogPost.module.css"
 
 
 const BlogPost = ({data}) => {
-    // const image = getImage(data.mdx.frontmatter.hero_image);
     return (
         <div>
             <Header/>
@@ -20,17 +18,6 @@ const BlogPost = ({data}) => {
                 <br/> <br/>
                 <div className={title}>{data.mdx.frontmatter.title}</div>
                 <div className={datePosted}>Date posted: {data.mdx.frontmatter.date}</div>
-
-                {/* <GatsbyImage
-                    image = {image}
-                    alt={data.mdx.frontmatter.hero_image_alt}
-                />
-                <p>
-                    Photo credit: {" "} 
-                    <a href={data.mdx.frontmatter.hero_image_credit_link}>
-                        {data.mdx.frontmatter.hero_image_credit_text}
-                    </a>
-                </p> */}
                 <p className={text}>
                 <MDXRenderer>
                     {data.mdx.body}
