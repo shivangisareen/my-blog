e43ad2e58390fb481bc0dc1f09ff6e6e20594a9c
Shivangi Sareen <s_sareen@apple.com>
2021-12-25 01:01:16 +0000

    add basic working dark mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
             }
         },
         "gatsby-plugin-mdx",
-        "gatsby-transformer-sharp"
+        "gatsby-transformer-sharp",
+        'gatsby-plugin-dark-mode'
     ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,21 @@ import * as React from "react";
 
 import NavBar from "../components/navBar";
 import Header from "../components/header";
+import Layout from "../components/Layout";
 import me from "../images/me.jpeg";
 
 import { intro, softwareEngineer, circularSquare } from "./index.module.css";
 
 const IndexPage = () => {
     return (
-      <div>
+      <Layout>
           <Header/>
           <NavBar/>
           <br/>
           <h2 className={intro}>hi! I'm a <span className={softwareEngineer}>software engineer</span> living in the UK!</h2>
           <br/>
           <img className={circularSquare} src={me} alt="This is me!"/>
-      </div>
+      </Layout>
     );
 };
 
