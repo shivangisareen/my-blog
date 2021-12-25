c4f0591c94f04400b67981466b737f6160fde6a4
Shivangi Sareen <s_sareen@apple.com>
2021-12-25 18:29:48 +0000

    fix warning for <a>

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,10 +15,10 @@ const ContactPage = () => {
             <br/>
             <h2 className={text}>find me here</h2>
             <div className={links}>
-                <a href="https://www.linkedin.com/in/shivangisareen/" target="_blank">linkedin</a> {"\t"}
-                <a href="https://twitter.com/SareenShivangi" target="_blank">twitter</a> {"\t"}
-                <a href="https://medium.com/@shivangisareen" target="_blank">medium</a> {"\t"}
-                <a href="https://shivangisareen.portfoliobox.net" target="_blank">portfolio</a>
+                <a href="https://www.linkedin.com/in/shivangisareen/" target="_blank" rel="noreferrer">linkedin</a> {"\t"}
+                <a href="https://twitter.com/SareenShivangi" target="_blank" rel="noreferrer">twitter</a> {"\t"}
+                <a href="https://medium.com/@shivangisareen" target="_blank" rel="noreferrer">medium</a> {"\t"}
+                <a href="https://shivangisareen.portfoliobox.net" target="_blank" rel="noreferrer">portfolio</a>
             </div>
         </Layout>
     );
