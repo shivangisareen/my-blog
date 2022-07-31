0a62080afb407c0030573da7ea31fb434a0be400
Shivangi Sareen <s_sareen@apple.com>
2022-07-31 21:46:22 +0100

    use BlogCard on blog page, autoformat

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,56 +1,56 @@
 import * as React from "react";
-import { graphql, Link } from 'gatsby';
+import { graphql, Link } from "gatsby";
+import Grid from "@mui/material/Grid";
 
 import NavBar from "../../components/navBar";
-import Header from "../../components/header";
+import Header from "../../components/Header";
 import Layout from "../../components/Layout";
-import { container, title, list } from "./styling/blogList.module.css";
-import "../../components/styling/global.css"
+import BlogCard from "../../components/BlogCard";
+import { container, title } from "./styling/blogList.module.css";
+import "../../components/styling/global.css";
 
-const Blog = ({data}) => {
-    return (
-        <Layout>
-            <Header/>
-            <NavBar/> 
-            <br/>
-            <br/>
-            <div className={container}>
-              <ul className={list}>
-                {
-                  data.allMdx.nodes.map(node => (
-                      <div key={node.id}>
-                        <li style={{content: "•", color: 'var(--textNormal)'}}>
-                          <Link to={node.slug} 
-                                className={title} 
-                                style={{color: 'var(--textNormal)',}}
-                          >
-                                {node.frontmatter.title}
-                          </Link>
-                        </li>
-                        <br/>
-                      </div>
-                  ))
-                }
-                </ul>
-            </div> 
-        </Layout>
-    );
+const Blog = ({ data }) => {
+  return (
+    <Layout>
+      <Header />
+      <NavBar />
+      <div className={container}>
+        <Grid container spacing={1} justify="center">
+          {data.allMdx.nodes.map((node) => (
+            <Grid item xs={12} sm={6} md={6} xl={4}>
+              <Link
+                to={node.slug}
+                className={title}
+                style={{ color: "var(--textNormal)" }}
+              >
+                <BlogCard
+                  title={node.frontmatter.title}
+                  summary={node.frontmatter.summary}
+                  date={node.frontmatter.date}
+                ></BlogCard>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Layout>
+  );
 };
 
 export const query = graphql`
-    query {
-        allMdx(sort: {order: DESC, fields: frontmatter___date}) {
-          nodes {
-            frontmatter {
-              title
-              date(formatString: "DD-MMM-YYYY")
-              updated(formatString: "DD-MMM-YYYY")
-            }
-            id
-            slug
-          }
+  query {
+    allMdx(sort: { order: DESC, fields: frontmatter___date }) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "DD-MMM-YYYY")
+          summary
         }
+        id
+        slug
       }
-`
+    }
+  }
+`;
 
 export default Blog;
